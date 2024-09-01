abc0cd6746257ee2e33123b31709901bd1373780
ronykris <rony.kris@gmail.com>
2024-09-01 19:05:24 +0530

    update

--- setup.py
+++ setup.py
@@ -3,12 +3,14 @@ from setuptools import setup, find_packages
 setup(
     name="stock_prediction_api",
     version="0.1",
-    packages=find_packages(exclude=["tests"]),
+    packages=find_packages(),
     install_requires=[
         "fastapi",
+        "uvicorn",
         "pydantic",
         "pandas",
         "yfinance",
         "prophet",
+        "pytest"
     ],
 )
